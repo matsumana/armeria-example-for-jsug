fdf5774d668e6c54c4fa72f2ce595b86b507c0a4
Manabu Matsuzaki <matsumana@users.noreply.github.com>
2020-07-20 00:12:12 +0900

    Add example projects (#1)
    - armeria-example-project1: REST API sever
    - armeria-example-project2: gRPC sever

---
+++ armeria-example-project2/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Component;
+
+import com.example.demo.grpc.Hello.HelloReply;
+import com.example.demo.grpc.Hello.HelloRequest;
+import com.example.demo.grpc.HelloServiceGrpc.HelloServiceImplBase;
+
+import io.grpc.stub.StreamObserver;
+
+@Component
+public class HelloController extends HelloServiceImplBase {
+
+    @Override
+    public void hello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        final HelloReply response = HelloReply.newBuilder()
+                                              .setMessage("こんにちは, " + request.getName())
+                                              .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
